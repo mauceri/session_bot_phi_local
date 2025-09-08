0b3a9d7e86ad9e3087a21ae4c1be3c62ef71effc
Christian Mauceri <cmauceri@gmail.com>
2025-09-08 17:34:16 +0200

    llama.cpp

--- phi_local/phi_local.py
+++ phi_local/phi_local.py
@@ -39,8 +39,9 @@ class Phi(IObserver):
             stime = time.time()
             reponse = self.il.interroge_llm(utilisateur, question);
             logger.info(f"Réponse du LLM \"{reponse}\"")
-            #reponse = reponse.choices[0].message.content
-            reponse = reponse['message']['content']
+            logger.info(f"***************************** Réponse du LLM \"{.choices[0]}\"")
+            reponse = reponse.choices[0].message.content
+            #reponse = reponse['message']['content']
             logger.info(f"Voici la réponse: {reponse}")
             self.il.sqliteh.modification_reponse(utilisateur, transaction_id,reponse)
         except BaseException as e:
